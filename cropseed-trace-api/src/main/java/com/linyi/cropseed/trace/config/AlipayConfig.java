5335a4c8fa548f4fb2898f6f45bd40bee466dbc4
L382611 <14131958+hsdchb@user.noreply.gitee.com>
2025-10-25 17:56:35 +0800

    feat(config): 添加支付宝配置类
    - 创建 AlipayConfig 配置类
    - 配置应用ID、私钥、支付宝公钥等属性
    - 设置默认字符集为UTF-8
    - 设置默认签名类型为RSA2
    - 配置异步通知和同步返回URL
    - 使用@ConfigurationProperties绑定配置属性

---
+++ cropseed-trace-api/src/main/java/com/linyi/cropseed/trace/config/AlipayConfig.java
@@ -0,0 +1,57 @@
+package com.linyi.cropseed.trace.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 支付宝配置
+ * 
+ * @author LinYi
+ * @since 2025-10-25
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "pay.alipay")
+public class AlipayConfig {
+
+    /**
+     * 应用ID
+     */
+    private String appId;
+
+    /**
+     * 私钥
+     */
+    private String privateKey;
+
+    /**
+     * 支付宝公钥
+     */
+    private String alipayPublicKey;
+
+    /**
+     * 服务器地址
+     */
+    private String serverUrl;
+
+    /**
+     * 字符集
+     */
+    private String charset = "UTF-8";
+
+    /**
+     * 签名类型
+     */
+    private String signType = "RSA2";
+
+    /**
+     * 异步通知URL
+     */
+    private String notifyUrl;
+
+    /**
+     * 同步返回URL
+     */
+    private String returnUrl;
+}
