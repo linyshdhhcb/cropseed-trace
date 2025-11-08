74d9a8ef19f6fc34174afb431bf8f7f31d8a34c1
L382611 <14131958+hsdchb@user.noreply.gitee.com>
2025-11-08 13:11:03 +0800

    feat(miniapp): 初始化小程序页面结构和路由配置

--- cropseed-trace-miniapp/main.js
+++ cropseed-trace-miniapp/main.js
@@ -1,22 +1,13 @@
-import App from './App'
+import App from "./App";
+import { createSSRApp } from "vue";
+import { createPinia } from "pinia";
+import "./uni.promisify.adaptor";
 
-// #ifndef VUE3
-import Vue from 'vue'
-import './uni.promisify.adaptor'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-  ...App
-})
-app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
+  const app = createSSRApp(App);
+  const pinia = createPinia();
+  app.use(pinia);
   return {
-    app
-  }
+    app,
+  };
 }
-// #endif
